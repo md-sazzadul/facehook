e50095921734f8b123c36e9ca287ea056e2656ae
md-sazzadul <md.sazzadul.islam15@gmail.com>
2024-03-14 14:01:22 +0100

    added post feed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,46 @@
-import React from "react";
-import { useAuth } from "../hooks/useAuth";
+import { useEffect, useReducer } from "react";
+import { actions } from "../actions";
+import PostList from "../components/posts/PostList";
+import useAxios from "../hooks/useAxios";
+import { initialState, postReducer } from "../reducers/PostReducer";
 
 const HomePage = () => {
-  const { auth } = useAuth();
-  console.log(auth);
+  const [state, dispatch] = useReducer(postReducer, initialState);
+
+  const { api } = useAxios();
+
+  useEffect(() => {
+    dispatch({ type: actions.post.DATA_FETCHING });
+
+    const fetchPost = async () => {
+      try {
+        const response = await api.get(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/posts`
+        );
+
+        if (response.status === 200) {
+          dispatch({ type: actions.post.DATA_FETCHED, data: response.data });
+        }
+      } catch (error) {
+        console.error(error);
+        dispatch({ type: actions.post.DATA_FETCH_ERROR, data: error.message });
+      }
+    };
+
+    fetchPost();
+  }, []);
+
+  if (state?.loading) {
+    return <div>We are working...</div>;
+  }
+
+  if (state?.error) {
+    return <div> Error in fetching posts {state?.error?.message}</div>;
+  }
+
   return (
     <div>
-      <p>HomePage</p>
+      <PostList posts={state?.posts} />
     </div>
   );
 };
